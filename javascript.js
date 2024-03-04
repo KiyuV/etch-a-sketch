e02903daa7d1c666ae1098be7e87f5d47ab56673
Samson Wu <160843158+KiyuV@users.noreply.github.com>
2024-03-04 16:33:58 +0200

    feat: add 'Set Size' and 'Reset' button

--- javascript.js
+++ javascript.js
@@ -1,7 +1,7 @@
 const mainContainer = document.querySelector('#main-container');
 
 function createGrid(height, width) {
-    let divHeight = 100/height;
+    let divHeight = 97/height;
     let divWidth = 100/width;
     let body = document.querySelector('body');
     for(let i = 0; i < height; i++) {
@@ -15,13 +15,37 @@ function createGrid(height, width) {
             let tmpDiv = document.createElement('div');
             tmpDiv.style.height = `${divHeight}vh`;
             tmpDiv.style.width = `${divWidth}vw`;
+            tmpDiv.className = 'pixel';
             tmpDiv.addEventListener('mouseover', () => {
                 tmpDiv.style.backgroundColor = `orange`;
             });
             tmpContainer.appendChild(tmpDiv);
-            body.appendChild(tmpContainer);
+            mainContainer.appendChild(tmpContainer);
         }
     }
 }
 
-createGrid(16,16);
+// Reset button to reset the canvas while keeping the resolution
+const resetBtn = document.querySelector('#reset');
+resetBtn.addEventListener('click', () => {
+    const allPixels = document.querySelectorAll('.pixel');
+    allPixels.forEach((pixel) => {
+        if (pixel.style.backgroundColor == 'orange') {
+            pixel.style.backgroundColor = 'white';
+        } 
+    })
+});
+
+
+const setSize = document.querySelector('#canvasSize');
+setSize.addEventListener('click', () => {
+    let size = Number(prompt('Please enter the size of your canvas (Max 100):'));
+    while (size > 100 || size < 0 || isNaN(size)) {
+        size = prompt('Please enter a valid size (0 - 100):');
+        if (size <= 100 && size >= 0 && !isNaN(size)) {
+            break;
+        }
+    };
+    mainContainer.innerHTML = '';
+    createGrid(size, size);
+});
