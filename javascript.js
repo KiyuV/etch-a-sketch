427de3b62d26e6f55b4c773ae93c18195294dfe0
Samson Wu <160843158+KiyuV@users.noreply.github.com>
2024-03-04 15:26:31 +0200

    feat: create square divs to the given dimensions

---
+++ javascript.js
@@ -0,0 +1,24 @@
+const mainContainer = document.querySelector('#main-container');
+
+function createGrid(height, width) {
+    let divHeight = 100/height;
+    let divWidth = 100/width;
+    let body = document.querySelector('body');
+    for(let i = 0; i < height; i++) {
+        let tmpContainer = document.createElement('div');
+        tmpContainer.style.display = 'flex';
+        tmpContainer.style.flex = '1';
+        tmpContainer.style.justifyContent = 'center';
+        tmpContainer.style.alignItems = 'center';
+
+        for(let j = 0; j < width; j++) {
+            let tmpDiv = document.createElement('div');
+            tmpDiv.style.height = `${divHeight}vh`;
+            tmpDiv.style.width = `${divWidth}vw`;
+            tmpContainer.appendChild(tmpDiv);
+            body.appendChild(tmpContainer);
+        }
+    }
+}
+
+createGrid(16,16);
