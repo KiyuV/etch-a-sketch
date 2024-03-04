2cea2301b4b0c916ee02753b57b675bc1a22aa6c
Samson Wu <160843158+KiyuV@users.noreply.github.com>
2024-03-04 19:54:14 +0200

    feat: add random-colored trail which darkens

--- javascript.js
+++ javascript.js
@@ -1,9 +1,9 @@
 const mainContainer = document.querySelector('#main-container');
+let coloredTile = 0;
 
 function createGrid(height, width) {
     let divHeight = 97/height;
     let divWidth = 100/width;
-    let body = document.querySelector('body');
     for(let i = 0; i < height; i++) {
         let tmpContainer = document.createElement('div');
         tmpContainer.style.display = 'flex';
@@ -17,7 +17,7 @@ function createGrid(height, width) {
             tmpDiv.style.width = `${divWidth}vw`;
             tmpDiv.className = 'pixel';
             tmpDiv.addEventListener('mouseover', () => {
-                tmpDiv.style.backgroundColor = `orange`;
+                tmpDiv.style.backgroundColor = getColor();
             });
             tmpContainer.appendChild(tmpDiv);
             mainContainer.appendChild(tmpContainer);
@@ -25,15 +25,27 @@ function createGrid(height, width) {
     }
 }
 
+function getColor() {
+    if (coloredTile < 10) {
+        let r = Math.floor(Math.random() * 256 * (1-coloredTile/10));
+        let g = Math.floor(Math.random() * 256 * (1-coloredTile/10));
+        let b = Math.floor(Math.random() * 256 * (1-coloredTile/10));
+        coloredTile++;
+        return `rgb(${r},${g},${b})`;
+    } else if (coloredTile >= 10) {
+        return 'rgb(0,0,0)'
+    }
+}
 // Reset button to reset the canvas while keeping the resolution
 const resetBtn = document.querySelector('#reset');
 resetBtn.addEventListener('click', () => {
     const allPixels = document.querySelectorAll('.pixel');
     allPixels.forEach((pixel) => {
-        if (pixel.style.backgroundColor == 'orange') {
-            pixel.style.backgroundColor = 'white';
+        if (pixel.style.backgroundColor != '') {
+            pixel.style.backgroundColor = '';
         } 
     })
+    coloredTile = 0;
 });
 
 
@@ -47,5 +59,7 @@ setSize.addEventListener('click', () => {
         }
     };
     mainContainer.innerHTML = '';
+    coloredTile = 0;
     createGrid(size, size);
 });
+
